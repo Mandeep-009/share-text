064d248138ff04f0ec17fb2dd1f990c68033ebd2
Mandeep <mandeepgahlawat009@gmail.com>
2023-12-22 14:43:08 +0530

    used dotenv package

--- backend/config.js
+++ backend/config.js
@@ -1,2 +1,7 @@
+import { configDotenv } from "dotenv";
+if(process.env.NODE_ENV !== 'production'){
+    configDotenv();
+}
+
 export const PORT = 5000;
 export const mongoDBURL = `${process.env.MONGODB_URL}`;
