de5334fdaf434170de51fc3a4b28e14b3f14195f
Mandeep Gahlawat <mandeepgahlawat@Mandeeps-MacBook-Air.local>
2025-03-16 12:03:57 +0530

    fixed authentication issue

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,6 @@
 import express from "express";
 import cors from "cors";
-import { PORT, mongoDBURL } from "./config.js";
-import Connection from "./models/connection.js";
+import { PORT} from "./config.js";
 
 const app = express();
 app.use(cors(
